7d5e4deb9ca2e7e40d044d5aa83d47e48d7197bf
Moe Kyaw Kyaw <moekyawkyaw.cumkm@gmail.com>
2022-06-24 17:38:20 +0630

    learning typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 24;
+age = 30;
+if (age > 50)
+    console.log("You are older than me");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let age: number = 24; // initialize age
+age = 30;
+if(age > 50)
+    console.log("You are older than me");
