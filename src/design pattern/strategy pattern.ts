64a032b7dd2edc67d080215e74c159e1f5e98442
Moe Kyaw Kyaw Khine <88232653+moekyawkyawkhine7@users.noreply.github.com>
2024-05-29 20:39:20 +0630

    composition & strategy design pattern is learned

---
+++ src/design pattern/strategy pattern.ts
@@ -0,0 +1,51 @@
+interface LoginStrategy {
+  login(username: string, password: string): void;
+}
+
+class EmailLogin implements LoginStrategy {
+  login(username: string, password: string) {
+    console.log(`Email Login: User Name: ${username}, Password: ${password}`);
+  }
+}
+
+class GoogleLogin implements LoginStrategy {
+  login(username: string, password: string): void {
+    console.log(`Google Login: User Name: ${username}, Password: ${password}`);
+  }
+}
+
+class FacebookLogin implements LoginStrategy {
+  login(username: string, password: string): void {
+    console.log(
+      `Facebook Login: User Name: ${username}, Password: ${password}`
+    );
+  }
+}
+
+class LoginSystem {
+  private strategy: LoginStrategy; // composition
+
+  constructor(strategy: LoginStrategy) {
+    this.strategy = strategy;
+  }
+
+  setSystem(strategy: LoginStrategy): void {
+    this.strategy = strategy;
+  }
+
+  loginProcess(username: string, password: string) {
+    this.strategy.login(username, password);
+  }
+}
+
+let googleLogin = new GoogleLogin();
+let loginSystem = new LoginSystem(googleLogin);
+// Perform Login
+loginSystem.loginProcess("Mg Mg", "11111111");
+// Change System
+let emailLogin = new EmailLogin();
+loginSystem.setSystem(emailLogin);
+// Perform Login
+loginSystem.loginProcess("Kyaw Kyaw", "11111111");
+loginSystem.setSystem(new FacebookLogin());
+loginSystem.loginProcess("Aung Aung", "11111111");
---
+++ src/oop/composition.ts
@@ -0,0 +1,38 @@
+class University {
+  private departments: Department[]; // composition
+
+  constructor(departments: Department[]) {
+    this.departments = departments;
+  }
+
+  addDepartment(department: Department) {
+    this.departments.push(department);
+  }
+
+  getDepartmentNames(): string[] {
+    return this.departments.map((department) => department.getName());
+  }
+}
+
+class Department {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+}
+
+class Hello{
+
+}
+
+let university = new University([
+  new Department("HR Department"),
+  new Department("Software Department"),
+]);
+university.addDepartment(new Department("Hardware Department"));
+console.log(university.getDepartmentNames());
